85c924fb88dca03be45d34e7f9e69727fc07155f
sushiogoto <yoshio.goto@gmail.com>
2015-12-29 20:55:49 -0800

    Partially complete user login

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,57 +1,40 @@
 import * as types from 'constants/auth';
 
 const initialState = {
-  showSuccessModal: false,
-  showErrorModal: false,
-  lastRequestUrl: null,
-  buttons: {}
+  isFetching: false
 };
 
-export default function authReducer(state = initialState, action)
+export default function authReducer (state = initialState, action) {
   switch (action.type) {
-    case types.REQUEST_FACEBOOK_STARTED:
-      return Object.assign({}, state, {
-        buttons: {
-          [key]: {
-            loading: true
-          }
-        }
-      });
+  case types.REQUEST_AUTH_STARTED:
+    return Object.assign({}, state, {
+  //     isFetching: true
+    });
 
-    case types.REQUEST_FACEBOOK_COMPLETED:
-      return Object.assign({}, state, {
-        buttons: {
-          [key]: {
-            loading: false
-          }
-        },
-        showSuccessModal: true,
-        lastRequestUrl: key
-      });
+  case types.REQUEST_AUTH_COMPLETED:
+    return Object.assign({}, state, {
+      isFetching: false,
+      data: action.auth
+    });
 
-    case types.REQUEST_FACEBOOK_ERROR:
-      return Object.assign({}, state, {
-        buttons: {
-          [key]: {
-            loading: false
-          }
-        },
-        showErrorModal: true,
-        lastRequestUrl: key
-      });
+  case types.REQUEST_AUTH_ERROR:
+    return Object.assign({}, state, {
+      // TODO: error handling
+    });
 
-    case types.DISMISS_REQUEST_FACEBOOK_SUCCESS_MODAL:
-      return Object.assign({}, state, {
-        showSuccessModal: false,
-        lastRequestUrl: null
-      });
+  case types.DISMISS_REQUEST_AUTH_SUCCESS_MODAL:
+    return Object.assign({}, state, {
+      showSuccessModal: false,
+      lastRequestUrl: null
+    });
 
-    case types.DISMISS_REQUEST_FACEBOOK_ERROR_MODAL:
-      return Object.assign({}, state, {
-        showErrorModal: false,
-        lastRequestUrl: null
-      });
+  case types.DISMISS_REQUEST_AUTH_ERROR_MODAL:
+    return Object.assign({}, state, {
+      showErrorModal: false,
+      lastRequestUrl: null
+    });
 
+  default:
+    return state;
   }
-
-});
+}
